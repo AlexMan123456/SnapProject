a3644d1d89cedee93eb5f23be6250fde1d59364c
AlexMan123456 <alex.man03888@gmail.com>
2025-02-04 15:32:39 +0000

    Deleted unnecessary comment in CardGame class

--- src/CardGame.java
+++ src/CardGame.java
@@ -6,7 +6,6 @@ public class CardGame {
 
     // CONSTRUCTOR
     public CardGame(){
-        //String[] symbol = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
         int[] values = {2,3,4,5,6,7,8,9,10,11,12,13,14};
         String[] suits = {"♠","♥","♦","♣"};
 
